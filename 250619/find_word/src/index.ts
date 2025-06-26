f8ca3372f188060fb4dd11b28cecdab182051a34
이수찬 <isuchan@isuchan-ui-MacBookPro.local>
2025-06-26 18:47:19 +0900

    [PROBLEM-CRUSHER] find_word - DFS pruning

--- 250619/find_word/src/index.ts
+++ 250619/find_word/src/index.ts
@@ -22,60 +22,70 @@ const pre = queries
 // Function to find words that start with a given prefix
 
 class Node {
-  value: string;
-  children: Map<string, Node>;
-  end: number = 0;
-  constructor(value = "") {
-    this.value = value;
-    this.children = new Map();
-  }
+	value: string;
+	children: Map<string, Node>;
+	end: number = 0;
+	constructor(value = '') {
+		this.value = value;
+		this.children = new Map();
+	}
 }
 
 class Trie {
-  root: Node;
+	root: Node;
 
-  constructor() {
-    this.root = new Node();
-  }
+	constructor() {
+		this.root = new Node();
+	}
 
-  insert(string: string) {
-    let node = this.root;
-    for (let i = 0; i < string.length; i++) {
-      if (!node.children.has(string[i])) {
-        node.children.set(string[i], new Node(node.value + string[i]));
-      }
-      node = node.children.get(string[i])!;
-    }
-	node.end++;
-  }
+	insert(string: string) {
+		let node = this.root;
+		for (let i = 0; i < string.length; i++) {
+			if (!node.children.has(string[i])) {
+				node.children.set(string[i], new Node(node.value + string[i]));
+			}
+			node = node.children.get(string[i])!;
+		}
+		node.end++;
+	}
 
-  // prefix로 시작하는 단어 개수
-  findWordsWithPrefix(prefix: string): string[] {
-    let node = this.root;
-    for (const char of prefix) {
-      if (!node.children.has(char)) return [];
-      node = node.children.get(char)!;
-    }
-    const results: [string, number][] = [];
-    const dfs = (cur: Node, path: string) => {
-      if (cur.end > 0) results.push([path, cur.end]);
-      for (const [char, child] of cur.children) {
-        dfs(child, path + char);
-      }
-    };
-    dfs(node, prefix);
+	// prefix로 시작하는 단어 개수
+	findWordsWithPrefix(prefix: string): string[] {
+		const maxValue = [];
+		let cnt = 0;
+		let node = this.root;
+		for (const char of prefix) {
+			if (!node.children.has(char)) return [];
+			node = node.children.get(char)!;
+		}
+		const results: [string, number][] = [];
+		const dfs = (cur: Node, path: string) => {
+			if (cur.end > 0) {
+				if (results.length < 10) {
+					results.push([path, cur.end]);
+				} else {
+					results.sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]));
+					if (cur.end > results[9][1]) {
+						results.pop();
+						results.push([path, cur.end]);
+					}
+				}
+			}
+			for (const [char, child] of cur.children) {
+				dfs(child, path + char);
+			}
+		};
+		dfs(node, prefix);
 
-    results.sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]));
-    return results.slice(0,10).map(([word]) => word); 
-  }
-
+		// results.sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]));
+		return results.slice(0, 10).map(([word]) => word);
+	}
 }
 
 const trie = new Trie();
-for(const w of words){
+for (const w of words) {
 	trie.insert(w);
 }
-
 
 function findWord(prefix: string): {
 	duration: number;
@@ -91,6 +101,10 @@ function findWord(prefix: string): {
 		result,
 	};
 }
-for(let i =1 ;i<pre.length;i++){
-	console.log('prefix : ',pre[i],findWord(pre[i]));
+for (let i = 1; i < pre.length; i++) {
+	console.log(
+		'prefix : ',
+		pre[i],
+		findWord(pre[i]).result.length > 0 ? findWord(pre[i]) : '**없음**'
+	);
 }
